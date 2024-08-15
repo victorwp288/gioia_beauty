b7c1f8985495e681311b4b9b46f85427af57efa9
Kai Aliani <kaialiani01@gmail.com>
2024-08-15 18:44:49 +0200

    added images to gallery and fixed bug

--- app/gallery/page.jsx
+++ app/gallery/page.jsx
@@ -1,14 +1,81 @@
 import Image from "next/image";
-import { demo1, demo2, demo3, demo4 } from "@/components/ImagesExports";
 import GalleryClient from "@/components/GalleryClient";
+import {
+  heroPicture,
+  reception1,
+  reception2,
+  reception3,
+  reception4,
+  mirror1,
+  mirror2,
+  pressoterapia1,
+  massaggi1,
+  ossigeno,
+  bagnoturco1,
+  bed,
+  rituali1,
+} from "@/components/ImagesExports";
 
 const Gallery = () => {
-  const images = [demo1, demo2, demo3, demo4, demo1, demo2, demo3, demo4];
+  const imagesWithDescriptions = [
+    {
+      src: reception1,
+      description: "Reception",
+    },
+    {
+      src: reception2,
+      description: "Reception",
+    },
+    {
+      src: reception3,
+      description: "Reception",
+    },
+    {
+      src: reception4,
+      description: "",
+    },
+    {
+      src: mirror1,
+      description: "",
+    },
+    {
+      src: mirror2,
+      description: "",
+    },
+    {
+      src: pressoterapia1,
+      description: "Pressoterapia",
+    },
+    {
+      src: massaggi1,
+      description: "",
+    },
+    {
+      src: ossigeno,
+      description: "Ossigeno Dermo Infusione",
+    },
+    {
+      src: bagnoturco1,
+      description: "Bagno turco",
+    },
+    {
+      src: bed,
+      description: "",
+    },
+    {
+      src: rituali1,
+      description: "Rituale Himalaya",
+    },
+  ];
+
   return (
     <div className="animate-fadeIn grid grid-cols-1 gap-1 sm:grid-cols-2 md:grid-cols-3 md:pt-16 lg:grid-cols-4">
-      {images.map((image, index) => (
+      {imagesWithDescriptions.map((image, index) => (
         <div key={index} className="overflow-hidden">
-          <GalleryClient images={images} index={index} />
+          <GalleryClient
+            imagesWithDescriptions={imagesWithDescriptions}
+            index={index} // Pass the current index here
+          />
         </div>
       ))}
     </div>
--- components/ImagesExports.jsx
+++ components/ImagesExports.jsx
@@ -1,43 +1,36 @@
-import maniPiedi from "@/images/feet.jpg";
-import rituale from "@/images/rituale.jpg";
-import ciglia from "@/images/ciglia.jpg";
-import bagnoTurcoPic from "@/images/bagno-turco.jpg";
 import tick from "@/images/tick.svg";
-import cerettaPic from "@/images/ceretta.jpg";
-import makeupPic from "@/images/makeup.jpg";
-import massaggiPic from "@/images/massaggio.jpg";
-import clock from "@/images/clock.svg";
-import elettroporatore from "@/images/elettroporazione.jpg";
 import whiteTick from "@/images/tick-white.svg";
 import viso from "@/images/viso.jpg";
-import demo1 from "@/images/demo_1.jpg";
-import demo2 from "@/images/demo_2.jpg";
-import demo3 from "@/images/demo_3.jpg";
-import demo4 from "@/images/demo_4.jpg";
-import portrait from "@/images/gioia-portrait.jpg";
-import portraitSquare from "@/images/gioia-portrait-square.jpg";
-import heart from "@/images/heart.svg";
+import reception1 from "@/images/reception1.jpg";
+import reception2 from "@/images/reception2.jpg";
+import reception3 from "@/images/reception3.jpg";
+import reception4 from "@/images/reception4.jpg";
+import mirror1 from "@/images/mirror1.jpg";
+import mirror2 from "@/images/mirror2.jpg";
+import pressoterapia1 from "@/images/pressoterapia1.jpg";
+import massaggi1 from "@/images/massaggi1.jpg";
+import ossigeno from "@/images/ossigeno.jpg";
+import bagnoturco1 from "@/images/bagnoturco1.jpg";
+import bed from "@/images/bed.jpg";
+import rituali1 from "@/images/rituali1.jpg";
 import loginPhoto from "@/images/login-photo.jpg";
+import heroPicture from "@/images/hero-picture.jpg";
 
 export {
-  maniPiedi,
-  rituale,
-  ciglia,
-  tick,
-  cerettaPic,
-  makeupPic,
-  massaggiPic,
-  clock,
-  elettroporatore,
   whiteTick,
-  demo1,
-  demo2,
-  demo3,
-  demo4,
-  portrait,
-  portraitSquare,
-  bagnoTurcoPic,
   viso,
-  heart,
   loginPhoto,
+  heroPicture,
+  reception1,
+  reception2,
+  reception3,
+  reception4,
+  mirror1,
+  mirror2,
+  pressoterapia1,
+  massaggi1,
+  ossigeno,
+  bagnoturco1,
+  bed,
+  rituali1,
 };
