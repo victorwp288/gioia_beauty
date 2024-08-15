160102ef8f68f8bc540eb376b08cf0ac52076c8a
Kai Aliani <kaialiani01@gmail.com>
2024-08-15 18:48:12 +0200

    no actually this is the one (sorry)

--- components/ImagesExports.jsx
+++ components/ImagesExports.jsx
@@ -1,6 +1,23 @@
+import maniPiedi from "@/images/feet.jpg";
+import rituale from "@/images/rituale.jpg";
+import ciglia from "@/images/ciglia.jpg";
+import bagnoTurcoPic from "@/images/bagno-turco.jpg";
 import tick from "@/images/tick.svg";
+import cerettaPic from "@/images/ceretta.jpg";
+import makeupPic from "@/images/makeup.jpg";
+import massaggiPic from "@/images/massaggio.jpg";
+import clock from "@/images/clock.svg";
+import elettroporatore from "@/images/elettroporazione.jpg";
 import whiteTick from "@/images/tick-white.svg";
 import viso from "@/images/viso.jpg";
+import portrait from "@/images/gioia-portrait.jpg";
+import portraitSquare from "@/images/gioia-portrait-square.jpg";
+import heart from "@/images/heart.svg";
+import loginPhoto from "@/images/login-photo.jpg";
+import heroPicture from "@/images/hero-picture.jpg";
+
+// gallery pictures
+
 import reception1 from "@/images/reception1.jpg";
 import reception2 from "@/images/reception2.jpg";
 import reception3 from "@/images/reception3.jpg";
@@ -13,13 +30,23 @@ import ossigeno from "@/images/ossigeno.jpg";
 import bagnoturco1 from "@/images/bagnoturco1.jpg";
 import bed from "@/images/bed.jpg";
 import rituali1 from "@/images/rituali1.jpg";
-import loginPhoto from "@/images/login-photo.jpg";
-import heroPicture from "@/images/hero-picture.jpg";
-import portraitSquare from "@/images/gioia-portrait-square.jpg";
 
 export {
+  maniPiedi,
+  rituale,
+  ciglia,
+  tick,
+  cerettaPic,
+  makeupPic,
+  massaggiPic,
+  clock,
+  elettroporatore,
   whiteTick,
+  portrait,
+  portraitSquare,
+  bagnoTurcoPic,
   viso,
+  heart,
   loginPhoto,
   heroPicture,
   reception1,
