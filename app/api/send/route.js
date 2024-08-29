bad9c8cde581bb46096d8f955c1ccb38b7b48cfb
kaialiani <111850398+kaialiani@users.noreply.github.com>
2024-08-29 18:00:59 +0200

    updated email template

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -17,19 +17,47 @@ export async function POST(request) {
       date,
     });
 
-    const { data, error } = await resend.emails.send({
-      from: "Gioia Beauty <noreply@gioiabeauty.net>",
-      to: [email],
-      subject: "Ricevuta di prenotazione",
-      react: EmailTemplate({ name, startTime, endTime, duration, date }),
-    });
+    // Send email to the customer
+    const { data: customerData, error: customerError } =
+      await resend.emails.send({
+        from: "Gioia Beauty <noreply@gioiabeauty.net>",
+        to: [email],
+        subject: "Ricevuta di prenotazione",
+        react: EmailTemplate({ name, startTime, endTime, duration, date }),
+      });
 
-    if (error) {
-      console.error("Error sending email:", error);
-      return new Response(JSON.stringify({ error }), { status: 500 });
+    if (customerError) {
+      console.error("Error sending email to customer:", customerError);
+      return new Response(JSON.stringify({ error: customerError }), {
+        status: 500,
+      });
     }
 
-    return new Response(JSON.stringify(data), { status: 200 });
+    // Send email to the admin (yourself)
+    const { data: adminData, error: adminError } = await resend.emails.send({
+      from: "Gioia Beauty <noreply@gioiabeauty.net>",
+      to: ["gioiabeautyy@gmail.com"],
+      subject: "Nuova prenotazione",
+      react: EmailTemplate({
+        name,
+        startTime,
+        endTime,
+        duration,
+        date,
+        isAdmin: true,
+      }),
+    });
+
+    if (adminError) {
+      console.error("Error sending email to admin:", adminError);
+      return new Response(JSON.stringify({ error: adminError }), {
+        status: 500,
+      });
+    }
+
+    return new Response(JSON.stringify({ customerData, adminData }), {
+      status: 200,
+    });
   } catch (error) {
     console.error("Catch error:", error);
     return new Response(JSON.stringify({ error: error.message }), {
--- components/EmailTemplate.jsx
+++ components/EmailTemplate.jsx
@@ -1,25 +1,51 @@
 // components/EmailTemplate.js
 import * as React from "react";
 
-export const EmailTemplate = ({ name, startTime, endTime, duration, date }) => (
+export const EmailTemplate = ({
+  name,
+  startTime,
+  endTime,
+  duration,
+  date,
+  isAdmin,
+}) => (
   <div>
-    <h1>Gentile {name},</h1>
-    <p>
-      Il tuo appuntamento nel centro estetico Gioia Beauty è stato confermato
-      per il giorno {date}.
-    </p>
-    <p>
-      <strong>Inizio appuntamento:</strong> {startTime}
-    </p>
-    <p>
-      <strong>Fine appuntamento:</strong> {endTime}
-    </p>
-    <p>
-      <strong>Durata:</strong> {duration} minuti
-    </p>
-
-    <p>
-      <i>Non è possibile rispondere a questa mail.</i>{" "}
-    </p>
+    {isAdmin ? (
+      <div>
+        <h1>Nuova Prenotazione</h1>
+        <p>
+          {name} ha prenotato un appuntamento per il giorno {date}.
+        </p>
+        <p>
+          <strong>Inizio appuntamento:</strong> {startTime}
+        </p>
+        <p>
+          <strong>Fine appuntamento:</strong> {endTime}
+        </p>
+        <p>
+          <strong>Durata:</strong> {duration} minuti
+        </p>
+      </div>
+    ) : (
+      <div>
+        <h1>Gentile {name},</h1>
+        <p>
+          Il tuo appuntamento nel centro estetico Gioia Beauty è stato
+          confermato per il giorno {date}.
+        </p>
+        <p>
+          <strong>Inizio appuntamento:</strong> {startTime}
+        </p>
+        <p>
+          <strong>Fine appuntamento:</strong> {endTime}
+        </p>
+        <p>
+          <strong>Durata:</strong> {duration} minuti
+        </p>
+        <p>
+          <i>Non è possibile rispondere a questa mail.</i>
+        </p>
+      </div>
+    )}
   </div>
 );
